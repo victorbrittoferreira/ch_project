7eaf4748b568f5972825164772d2ef8012debafe
Victor B Ferreira <victorbrittoferreira@gmail.com>
2024-02-16 13:07:59 -0300

    feat(base project): created django base structure to develop

---
+++ ch_customers/config.py
@@ -0,0 +1,36 @@
+from os.path import join, dirname
+import environ
+
+dotenv_path = join(dirname(__file__), ".env")
+
+class EnvConfig:
+    env = environ.Env()
+    environ.Env.read_env(dotenv_path)
+
+    @staticmethod
+    def get_secret_key() -> str:
+        return EnvConfig.env('SECRET_KEY')
+
+    @staticmethod
+    def get_debug() -> bool:
+        return EnvConfig.env.bool('DEBUG', default=False)
+    
+    @staticmethod
+    def get_db_port() -> int:
+        return EnvConfig.env.int('DB_PORT')
+
+    @staticmethod
+    def get_db_host() -> str:
+        return EnvConfig.env('DB_HOST')
+    
+    @staticmethod
+    def get_db_name() -> str:
+        return EnvConfig.env('DB_NAME')
+    
+    @staticmethod
+    def get_db_user() -> str:
+        return EnvConfig.env('DB_USER')
+    
+    @staticmethod
+    def get_db_password() -> str:
+        return EnvConfig.env('DB_PASSWORD')
---
+++ readme.md
@@ -0,0 +1,3 @@
+alertar o usuario a instalar
+
+a lib sudo apt-get install libpq-dev
